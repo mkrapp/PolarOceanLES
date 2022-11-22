035fa11aadf0348a5572c17ba5c1c72b2456e532
Mario Krapp <mariokrapp@gmail.com>
2022-11-23 12:38:40 +1300

    add function for z-level refinement

--- utils.jl
+++ utils.jl
@@ -10,3 +10,18 @@ function parse_experiment(experiment)
     end
     return parse_args(s)["experiment"]
 end
+
+function z_levels(Nz,Lz,z₀,refinement,stretching)
+	# Normalized height ranging from 0 to 1
+	@inline h(k) = (k - 1) / Nz
+	
+	# Linear near-surface generator
+	@inline ζ₀(k) = 1 + (h(k) - 1) / refinement
+	
+	# Bottom-intensified stretching function
+	@inline Σ(k) = (1 - exp(-stretching * h(k))) / (1 - exp(-stretching))
+	
+	# Generating function
+	@inline z_faces(k) = z₀ + Lz * (ζ₀(k) * Σ(k) - 1)
+	return z_faces
+end
